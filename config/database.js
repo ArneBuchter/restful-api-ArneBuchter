56f30346a790f4e1b087d5ef6313cdd4da8d3b72
Arne Emil Buchter <5651612@RTS-365.dk>
2019-11-15 09:33:33 +0100

    addSecret

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
-const firebase = require("firebase");
+module.exports = require("firebase").initializeApp({
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCN8alM0M9ZIcSsmgg4Lmi6LwKB8dCzHMI",
-    authDomain: "briansostebutikarne.firebaseapp.com",
-    databaseURL: "https://briansostebutikarne.firebaseio.com",
-    projectId: "briansostebutikarne",
-    storageBucket: "briansostebutikarne.appspot.com",
-    messagingSenderId: "902730866318",
-    appId: "1:902730866318:web:1278bb6d5e5e2a611273df"
-  };
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+});
 
-  const db = firebase.initializeApp(firebaseConfig);
 
-  module.exports = db;
+ 
+
+  
